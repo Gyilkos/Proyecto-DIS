bddb4a6ecc89fd91f86fec66e30a1ab3d4758a83
Nicolas Robles <nicolas.robles@alumnos.ucn.cl>
2023-11-13 23:47:59 -0300

    V0.2Nico

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -16,6 +16,15 @@ var reply2 = await client.CuadradoAsync(
 
 Console.WriteLine("Cuadrado de " + num + " = " + reply2.Cuadrado);
 
+// Solicitar al usuario que ingrese la patente
+Console.Write("Ingrese la patente del automóvil: ");
+string patente = Console.ReadLine();
+
+// Mostrar la patente ingresada
+Console.WriteLine("La patente ingresada es: " + patente);
+
+// Esperar a que el usuario presione Enter antes de cerrar la aplicación
+Console.ReadLine();
 
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
