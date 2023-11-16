fdaaefe79ff6b3ebe991a19686fcfb32e3d3eed3
CristobalOssesO <cristobal.osses@hotmail.com>
2023-11-15 21:24:40 -0300

    version15.11Cristobal
    Arriendo listo, falta menu, mantencion casi listo y falta menu

---
+++ sharedDB/arriendo.py
@@ -0,0 +1,116 @@
+# Imports
+import sqlite3
+
+# Datos Generales
+
+
+# *Mostrar lista (Disponibles)
+# Estado, Kilometraje
+def showList():
+    print("Show List")
+    # Realizar una función SELECT
+    con = sqlite3.connect("Shared.db")
+
+    cur = con.cursor()
+    cur.execute("SELECT Patente FROM Camioneta WHERE Estado = 1;")
+
+    respuestaQuery = cur.fetchall()
+
+    string = "("
+    for fila in respuestaQuery:
+        fila = str(fila).strip('(),')
+        string += fila + ","
+    string = str(string).strip(',')
+    string += ')'
+    #print(string)
+
+    cur.execute("SELECT Patente, Kilometraje FROM Mantencion WHERE Patente IN" + string + ';')
+    
+    respuestaQuery = cur.fetchall()
+    for fila in respuestaQuery:
+        columna1, columna2 = fila
+        print("Patente: " + str(columna1) + " | Kilometraje: " + str(columna2))
+
+
+
+    # Imprimir cada componente en bucle
+    
+    # Cerrar la conección
+    con.commit()
+    con.close()
+
+# *Arriendo Auto
+# Patente, Estado
+def rentCar():
+    print("Rent Car")
+    patenteArrendar = input("Ingrese la patente del auto para arrendar: ").upper()
+    autoDisponible = False
+    try:
+        #query para verificar que este la patente y el auto disponibles
+        con = sqlite3.connect("Shared.db")
+        cur = con.cursor()
+        cur.execute("SELECT Patente FROM Camioneta WHERE Estado = 1 AND Patente = '"+ patenteArrendar+ "';")
+        if len(cur.fetchall()) == 0:
+            print("ERROR:Revise que el vehiculo esta disponible(len=0)")
+        else:
+            autoDisponible = True
+        
+        con.commit()
+        con.close()
+    except:
+        print("ERROR:Revise que el vehiculo esta disponible(except)")
+    if autoDisponible == True:
+        #Hacer query para cambiar estado a Arrendado (2)
+        try:
+            con = sqlite3.connect("Shared.db")
+            cur = con.cursor()
+            cur.execute("UPDATE Camioneta SET Estado = 2 WHERE Patente = '"+patenteArrendar+"';")
+            con.commit()
+            con.close()
+            print("+"*10)
+            print("Arriendo de "+patenteArrendar+" realizado con exito")
+            print("+"*10)
+        except:
+            print("ERROR: hubo problemas con su arriendo intente denuevo")
+
+# *Devolver auto
+# Patente, Estado, Kilometraje (actualizado)
+def releaseCar():
+    print("Rent Car")
+    patenteArrendar = input("Ingrese la patente del auto para arrendar: ").upper()
+    autoDisponible = False
+    try:
+        #query para verificar que este la patente y el auto Arrendado
+        con = sqlite3.connect("Shared.db")
+        cur = con.cursor()
+        cur.execute("SELECT Patente FROM Camioneta WHERE Estado = 2 AND Patente = '"+ patenteArrendar+ "';")
+        if len(cur.fetchall()) == 0:
+            print("ERROR:Revise que el vehiculo esta disponible(len=0)")
+        else:
+            autoDisponible = True
+        
+        con.commit()
+        con.close()
+    except:
+        print("ERROR:Revise que el vehiculo esta disponible(except)")
+    if autoDisponible == True:
+        #Hacer query para cambiar estado a Disponible (1)
+        try:
+            con = sqlite3.connect("Shared.db")
+            cur = con.cursor()
+            cur.execute("UPDATE Camioneta SET Estado = 1 WHERE Patente = '"+patenteArrendar+"';")
+            con.commit()
+            con.close()
+            print("+"*10)
+            print("Entrega de "+patenteArrendar+" realizado con exito")
+            print("+"*10)
+        except:
+            print("ERROR: hubo problemas con su arriendo intente denuevo")
+
+# *Main
+# Menu
+def Menu():
+    print("Menu")
+
+#Testing
+showList()
---
+++ sharedDB/mantencion.py
@@ -0,0 +1,79 @@
+# Imports
+import sqlite3
+
+
+
+# *Mostrar lista
+# Patente, Estado, Kilometraje,ProximaMantencion
+def showList():
+    print("Show List")
+    #buscar patentes de autos disponibles y mantenimiento (estado: 0 y 1) y devolver las patentes
+    #print("peticion patentes y estados de no arrendadas")
+    con = sqlite3.connect("Shared.db")
+    cur = con.cursor()
+    cur.execute("SELECT Patente, Estado FROM Camioneta WHERE Estado = 0 OR Estado = 1;")
+    respuestaQuery = cur.fetchall()
+    respuesta = ""
+    for fila in respuestaQuery:
+        # fila es una tupla que contiene los valores de las columnas
+        columna1, columna2 = fila
+        respuesta += " "+str(columna1) + ","+ str(columna2) 
+        #print(f"Columna1: {columna1}, Columna2: {columna2}")
+    
+    respuesta = respuesta[1:]
+    respuesta = respuesta.split(" ")
+    matrizPatenteEstado = []
+    listaPatentes = []
+    for x in respuesta:
+        parDatos = x.split(",")
+        matrizPatenteEstado.append(parDatos)
+        listaPatentes.append(parDatos[0])
+    print(matrizPatenteEstado)
+    print(listaPatentes)
+
+
+    string = "("
+    for patente in listaPatentes:
+        
+        string += "'"+patente + "',"
+    string = str(string).strip(',')
+    string += ')'
+    print(string)
+    cur.execute("SELECT Patente, Kilometraje FROM Mantencion WHERE Patente IN" + string + ';')
+
+    con.commit()
+    con.close()
+
+# *Actualizar kilometros
+# Patente, Kilometraje
+def setKilometer(patente, newKilometer):
+    print("Set Kilometer")
+    # Realizar función SELECT para buscar auto
+    # Actualizar dato de la base de datos (Kilometraje = newKilometer)
+    # Cerrar la conección
+
+# *Actualizar proxima mantencion
+# Patente
+def setNextMaintence(patente, newNextMaintence):
+    print("Set Next Maintence")
+    # Realizar función SELECT para buscar auto
+    # Actualizar dato de la base de datos (nextMaintence = newNextMaintence)
+    # Cerrar la conección
+
+# *Iniciar mantencion
+# Patente, Estado
+def startMaintence(patente):
+    print("Start Maintence")
+    # Realizar función SELECT para buscar auto
+    # Actualizar dato de la base de datos (Estado => 1)
+    # Cerrar la conección
+
+# *Terminar mantencion
+# Patente, Estado
+def finishMaintence(patente):
+    print("Finish Maintence")
+    # Realizar función SELECT para buscar auto
+    # Actualizar dato de la base de datos (Estado => 0)
+    # Cerrar la conección
+
+showList()
