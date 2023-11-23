578add9c099c1186e316f308d22ade2831557ea1
CristobalOssesO <cristobal.osses@hotmail.com>
2023-11-23 20:57:40 -0300

    Update mysite

--- sharedDB/mysite/polls/views.py
+++ sharedDB/mysite/polls/views.py
@@ -2,10 +2,37 @@
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
+from django.shortcuts import render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+    ''' shortcut render
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+    '''
 
 def chao (request):
     return HttpResponse("Bye world")
+
+def detail(request, question_id):
+    return HttpResponse("Usted está en la pagina: %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
