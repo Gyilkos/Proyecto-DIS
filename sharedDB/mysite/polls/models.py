f4162757cd097ac9a8dbd1019069eff9bcba056b
CristobalOssesO <cristobal.osses@hotmail.com>
2023-12-19 13:01:31 -0300

    Update

--- sharedDB/mysite/polls/models.py
+++ sharedDB/mysite/polls/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 
 class Question(models.Model):
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
--- sharedDB/mysite/polls/views.py
+++ sharedDB/mysite/polls/views.py
@@ -3,7 +3,7 @@
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-
+from django.utils import timezone
 from django.http import Http404
 from django.views import generic
 
@@ -11,6 +11,15 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
 from .models import Choice, Question
+
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        :5
+    ]
 
 '''
 def index(request):
